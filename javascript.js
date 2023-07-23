18dbe4fb9a3c374963d272ce99ffddf0b821c861
mkaplan6 <mkaplan6@illinois.edu>
2023-07-23 18:53:14 -0500

    fixed some typos

--- javascript.js
+++ javascript.js
@@ -9,19 +9,19 @@ var quotes = [
     "\"Unless someone like you cares a whole awful lot, nothing is going to get better. It's not.\" -The Lorax",
     "\"It smells like updog in here.\" -unknown",
     "\"Live your life, Puss in Boots. Live it well.\" -Death",
-    "\"If you're going to trust anything, trust mergesort\" -unknown",
-    "\"It's some kind of sin to live your whole life on a \'might have been\' \" -Brandon Flowers, from \"Caution\" by The Killers",
+    "\"If you're going to trust anything, trust mergesort.\" -unknown",
+    "\"It's some kind of sin to live your whole life on a \'might have been.\' \" -Brandon Flowers, from \"Caution\" by The Killers",
     "\"What's the difference between a piano, a fish, and a hanger? You can tune a piano but you can't tuna fish. And I knew you'd be hung up on the hanger.\" -my high school calculus teacher",
     "\"Ah. I knew you'd come. To stand before the Elden Ring. To become Elden Lord\" -Sir Gideon Ofnir, the All-Knowing",
-    "\"With great power comes great responsibility\" -Uncle Ben, to Spider-Man",
+    "\"With great power comes great responsibility.\" -Uncle Ben, to Spider-Man",
     "\"Hand it over. That thing, your dark soul.\" -Slave Knight Gael",
-    "\"A skeleton walks into a bar and asks for a beer and a mop\" -unknown",
-    "\"Why can't you trust atoms? Because they make up everything\" -my high school chemistry teacher",
-    "\"A guy walks into a bar...and he was disqualified from the limbo contest\" -unknown",
-    "\"Ooh wee ooh I look just like Buddy Holly\" -River Cuomo, from \"Buddy Holly\" by Weezer",
+    "\"A skeleton walks into a bar and asks for a beer and a mop.\" -unknown",
+    "\"Why can't you trust atoms? Because they make up everything.\" -my high school chemistry teacher",
+    "\"A guy walks into a bar...and he was disqualified from the limbo contest.\" -unknown",
+    "\"Ooh wee ooh I look just like Buddy Holly.\" -River Cuomo, from \"Buddy Holly\" by Weezer",
     "\"Cowabunga dude, so let's get it on. Reptiles against the fathers of the Renaissance.\" -Leonardo Da Vinci, Epic Rap Battles of History",
     "\"Our greatest glory is not in never failing, but in rising every time we fall.\" -Confucius",
-    "\"I shall find a way or make one\" -Hannibal"
+    "\"I shall find a way or make one.\" -Hannibal"
 ]
 
 function generateQuote() {
